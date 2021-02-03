5aa9766361f9840f7fd177ae011ce7fce43776ca
samcolson4 <samcolson4@gmail.com>
2021-02-03 13:55:45 +0000

    Add favicon

--- catan/src/components/generator/Generator-logic.js
+++ catan/src/components/generator/Generator-logic.js
@@ -7,7 +7,10 @@ class GeneratorLogic {
   }
 
   shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 
   allocateHexes() {
