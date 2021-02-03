9db68b72d0b8668d163df13be3e1e3a285b54c73
samcolson4 <samcolson4@gmail.com>
2021-02-03 10:22:12 +0000

    Add Generator to app

--- catan/src/components/App.js
+++ catan/src/components/App.js
@@ -1,9 +1,11 @@
 import Header from './header/Header.js'
+import Generator from './generator/Generator.js'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Generator />
     </div>
   )}
 
---
+++ catan/src/components/generator/Generator.js
@@ -0,0 +1,8 @@
+function Generator() {
+  return (
+    <div className="Generator">
+      <p>Where the board will go</p>
+    </div>
+  )}
+
+export default Generator;
