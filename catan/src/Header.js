b5814c1cded6bf499fc9e53df4dacfdd4bcfaa70
samcolson4 <samcolson4@gmail.com>
2021-02-03 10:10:50 +0000

    Add test header.

---
+++ catan/src/Header.js
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="Header-div">
+      <header className="Header">
+        <h1>
+          This is a header.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
