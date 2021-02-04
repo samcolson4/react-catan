c82bce428d82786baefcacd8d53b4ebe1df6fba3
samcolson4 <samcolson4@gmail.com>
2021-02-04 19:27:28 +0000

    Update

--- catan/src/components/generator/imageGenerator/imageGenerator.js
+++ catan/src/components/generator/imageGenerator/imageGenerator.js
@@ -33,35 +33,35 @@ function ImageGenerator() {
       <br></br>
 
       <div id="row_one">
-          { generator.boardHexes[0] } [{ generator.boardNumbers[0] }],  
-          { generator.boardHexes[1] } [{ generator.boardNumbers[1] }], 
+          { generator.boardHexes[0] } [{ generator.boardNumbers[0] }]  
+          { generator.boardHexes[1] } [{ generator.boardNumbers[1] }] 
           { generator.boardHexes[2] } [{ generator.boardNumbers[2] }] 
       </div>
       
       <div id="row_two">
         <p> 
-          { generator.boardHexes[3] } [{ generator.boardNumbers[3] }],  
-          { generator.boardHexes[4] } [{ generator.boardNumbers[4] }],  
-          { generator.boardHexes[5] } [{ generator.boardNumbers[5] }],
+          { generator.boardHexes[3] } [{ generator.boardNumbers[3] }]
+          { generator.boardHexes[4] } [{ generator.boardNumbers[4] }]  
+          { generator.boardHexes[5] } [{ generator.boardNumbers[5] }]
           { generator.boardHexes[6] } [{ generator.boardNumbers[6] }]
         </p>
       </div>
 
       <div id="row_three">
         <p> 
-          { generator.boardHexes[7] } [{ generator.boardNumbers[7] }],  
-          { generator.boardHexes[8] } [{ generator.boardNumbers[8] }],  
-          { generator.boardHexes[9] } [{ generator.boardNumbers[9] }],
-          { generator.boardHexes[10] } [{ generator.boardNumbers[10] }],
+          { generator.boardHexes[7] } [{ generator.boardNumbers[7] }] 
+          { generator.boardHexes[8] } [{ generator.boardNumbers[8] }]  
+          { generator.boardHexes[9] } [{ generator.boardNumbers[9] }]
+          { generator.boardHexes[10] } [{ generator.boardNumbers[10] }]
           { generator.boardHexes[11] } [{ generator.boardNumbers[11] }]
         </p>
       </div>
 
       <div id="row_four">
         <p> 
-          { generator.boardHexes[12] } [{ generator.boardNumbers[12] }],  
-          { generator.boardHexes[13] } [{ generator.boardNumbers[13] }],  
-          { generator.boardHexes[14] } [{ generator.boardNumbers[14] }],
+          { generator.boardHexes[12] } [{ generator.boardNumbers[12] }]  
+          { generator.boardHexes[13] } [{ generator.boardNumbers[13] }]  
+          { generator.boardHexes[14] } [{ generator.boardNumbers[14] }]
           { generator.boardHexes[15] } [{ generator.boardNumbers[15] }]
         </p>
       </div>
