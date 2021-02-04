65e601ec430ad2297ceec9204307074ba30dcbb9
samcolson4 <samcolson4@gmail.com>
2021-02-04 21:29:58 +0000

    Add numbers

--- catan/src/components/generator/imageGenerator/imageGenerator.js
+++ catan/src/components/generator/imageGenerator/imageGenerator.js
@@ -11,27 +11,40 @@ import sheepImp from './hexes/sheep.png'
 import twoImp from './numbers/two.png'
 import threeImp from './numbers/three.png'
 import fourImp from './numbers/four.png'
+import fiveImp from './numbers/five.png'
+import sixImp from './numbers/six.png'
+import eightImp from './numbers/eight.png'
+import nineImp from './numbers/nine.png'
+import tenImp from './numbers/ten.png'
+import elevenImp from './numbers/eleven.png'
+import twelveImp from './numbers/twelve.png'
+
 
 function ImageGenerator() {
 
-  let wheat = <img src={wheatImp} />
-  let brick = <img src={brickImp} />
-  let desert = <img src={desertImp} />
-  let lumber = <img src={lumberImp} />
-  let ore = <img src={oreImp} />
-  let sheep = <img src={sheepImp} />
+  let wheat = <img src={wheatImp} class="hex"/>
+  let brick = <img src={brickImp} class="hex"/>
+  let desert = <img src={desertImp} class="hex"/>
+  let lumber = <img src={lumberImp} class="hex"/>
+  let ore = <img src={oreImp} class="hex"/>
+  let sheep = <img src={sheepImp} class="hex"/>
 
-  let two = <img src={twoImp} />
-  let three = <img src={threeImp} />
-  let four = <img src={fourImp} />
-
-
+  let two = <img src={twoImp} class="number_img"/>
+  let three = <img src={threeImp} class="number_img"/>
+  let four = <img src={fourImp} class="number_img"/>
+  let five = <img src={fiveImp} class="number_img"/>
+  let six = <img src={sixImp} class="number_img"/>
+  let eight = <img src={eightImp} class="number_img"/>
+  let nine = <img src={nineImp} class="number_img"/>
+  let ten = <img src={tenImp} class="number_img"/>
+  let eleven = <img src={elevenImp} class="number_img"/>
+  let twelve = <img src={twelveImp} class="number_img"/>
 
   let hexes = [wheat, wheat, wheat, ore, ore, ore, brick, brick, brick, brick, sheep, sheep, sheep, sheep, lumber, lumber, lumber, lumber, desert]
 
   let numbers = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11]
-  
-  let num_imgs = [5, two, 6, three, 8, 10, 9, 12, 11, four, 8, 10, 9, four, 5, 6, three, 11]
+
+  let num_imgs = [five, two, six, three, eight, ten, nine, twelve, eleven, four, eight, ten, nine, four, five, six, three, eleven]
 
   let ports = ["sheep", "lumber", "ore", "wheat", "three", "three", "three", "three"]
 
@@ -46,7 +59,7 @@ function ImageGenerator() {
       <div id="board">
         <div class= "row" id="row_one">
             { generator.boardHexes[0] } 
-            <span class="number_img">{ generator.boardNumbers[0] }</span>
+            { generator.boardNumbers[0] }
             { generator.boardHexes[1] } 
             { generator.boardNumbers[1] }
             { generator.boardHexes[2] } 
