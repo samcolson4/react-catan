be3ceec1baa86e1e05f431878ba786fedfb0762c
samcolson4 <samcolson4@gmail.com>
2021-02-04 19:54:15 +0000

    Remove paragraph tags

--- catan/src/components/generator/imageGenerator/imageGenerator.js
+++ catan/src/components/generator/imageGenerator/imageGenerator.js
@@ -33,45 +33,37 @@ function ImageGenerator() {
       <br></br>
 
       <div id="row_one">
-          { generator.boardHexes[0] } [{ generator.boardNumbers[0] }]  
+          { generator.boardHexes[0] } [{ generator.boardNumbers[0] }]
           { generator.boardHexes[1] } [{ generator.boardNumbers[1] }] 
           { generator.boardHexes[2] } [{ generator.boardNumbers[2] }] 
       </div>
       
       <div id="row_two">
-        <p> 
           { generator.boardHexes[3] } [{ generator.boardNumbers[3] }]
           { generator.boardHexes[4] } [{ generator.boardNumbers[4] }]  
           { generator.boardHexes[5] } [{ generator.boardNumbers[5] }]
           { generator.boardHexes[6] } [{ generator.boardNumbers[6] }]
-        </p>
       </div>
 
       <div id="row_three">
-        <p> 
           { generator.boardHexes[7] } [{ generator.boardNumbers[7] }] 
           { generator.boardHexes[8] } [{ generator.boardNumbers[8] }]  
           { generator.boardHexes[9] } [{ generator.boardNumbers[9] }]
           { generator.boardHexes[10] } [{ generator.boardNumbers[10] }]
           { generator.boardHexes[11] } [{ generator.boardNumbers[11] }]
-        </p>
       </div>
 
       <div id="row_four">
-        <p> 
           { generator.boardHexes[12] } [{ generator.boardNumbers[12] }]  
           { generator.boardHexes[13] } [{ generator.boardNumbers[13] }]  
           { generator.boardHexes[14] } [{ generator.boardNumbers[14] }]
           { generator.boardHexes[15] } [{ generator.boardNumbers[15] }]
-        </p>
       </div>
 
       <div id="row_five">
-        <p> 
           { generator.boardHexes[16] } [{ generator.boardNumbers[16] }],  
           { generator.boardHexes[17] } [{ generator.boardNumbers[17] }], 
           { generator.boardHexes[18] } [{ generator.boardNumbers[18] }] 
-        </p>
       </div>
       <br></br>
     </div>
