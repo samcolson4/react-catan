76849c49027d8f888f03678cd10bfbd1c4c01e58
samcolson4 <samcolson4@gmail.com>
2021-02-04 17:28:16 +0000

    Add image generator

--- catan/src/components/App.js
+++ catan/src/components/App.js
@@ -1,5 +1,6 @@
 import Header from './header/Header.js'
-import TextGenerator from './generator/textGenerator/Generator.js'
+import TextGenerator from './generator/textGenerator/textGenerator.js'
+import ImageGenerator from './generator/imageGenerator/imageGenerator.js'
 import About from './about/About.js'
 import './app.css'
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <TextGenerator />
+      <ImageGenerator />
       <About />
     </div>
   )}
