c7628216d81143abee91fe83b6a8617c236e7353
samcolson4 <samcolson4@gmail.com>
2021-02-04 21:14:41 +0000

    two

--- catan/src/components/generator/imageGenerator/imageGenerator.js
+++ catan/src/components/generator/imageGenerator/imageGenerator.js
@@ -1,11 +1,14 @@
 import GeneratorLogic from '../Generator-logic.js'
 import './imageGenerator.css'
+
 import wheatImp from './hexes/wheat.png'
 import brickImp from './hexes/brick.png'
 import desertImp from './hexes/desert.png'
 import lumberImp from './hexes/lumber.png'
 import oreImp from './hexes/ore.png'
 import sheepImp from './hexes/sheep.png'
+
+import twoImp from './numbers/two.png'
 
 function ImageGenerator() {
 
@@ -16,9 +19,11 @@ function ImageGenerator() {
   let ore = <img src={oreImp} />
   let sheep = <img src={sheepImp} />
 
+  let two = <img src={twoImp} />
+
   let hexes = [wheat, wheat, wheat, ore, ore, ore, brick, brick, brick, brick, sheep, sheep, sheep, sheep, lumber, lumber, lumber, lumber, desert]
 
-  let numbers = [5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11]
+  let numbers = [5, two, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11]
 
   let ports = ["sheep", "lumber", "ore", "wheat", "three", "three", "three", "three"]
 
@@ -32,9 +37,12 @@ function ImageGenerator() {
       <br></br>
       <div id="board">
         <div class= "row" id="row_one">
-            { generator.boardHexes[0] } [{ generator.boardNumbers[0] }]
-            { generator.boardHexes[1] } [{ generator.boardNumbers[1] }] 
-            { generator.boardHexes[2] } [{ generator.boardNumbers[2] }] 
+            { generator.boardHexes[0] } 
+            <span class="number_img">{ generator.boardNumbers[0] }</span>
+            { generator.boardHexes[1] } 
+            { generator.boardNumbers[1] }
+            { generator.boardHexes[2] } 
+            { generator.boardNumbers[2] }
         </div>
         
         <div class= "row" id="row_two">
