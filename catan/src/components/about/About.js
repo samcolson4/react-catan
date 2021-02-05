6340142f22def67124280b83c0e6739293a58934
samcolson4 <samcolson4@gmail.com>
2021-02-05 09:50:48 +0000

    Update about

--- catan/src/components/about/About.js
+++ catan/src/components/about/About.js
@@ -4,7 +4,13 @@ function About() {
   return (
     <div className="About-div">
       <hr></hr>
-      <p> This is an app made by Sam Colson.</p>
+      <h1>About</h1>
+      <p> This is an app by <a href="http://colsondesign.com">Sam Colson</a>, built to learn the basics of React.</p>
+
+      <br></br>
+      <p><a href="https://github.com/samcolson4/react-catan">Project github repo</a></p>
+      <p><a href="https://github.com/samcolson4/">Github profile</a></p>
+      <p><a href="https://www.linkedin.com/samcolson">Linkedin</a></p>
     </div>
   );
 }
