4330e59680f391df1231755773332b46bf693ea0
samcolson4 <samcolson4@gmail.com>
2021-02-07 15:12:15 +0000

    Update links

--- catan/src/components/about/About.js
+++ catan/src/components/about/About.js
@@ -5,12 +5,10 @@ function About() {
     <div className="About-div">
       <hr></hr>
       <h1>About</h1>
-      <p> This is an app by <a href="http://colsondesign.com">Sam Colson</a>, built to learn the basics of React.</p>
+      <p> This is an app by <a href="https://github.com/samcolson4/">Sam Colson</a>, built to learn the basics of React.</p>
 
       <br></br>
-      <p><a href="https://github.com/samcolson4/react-catan">Project github repo</a></p>
-      <p><a href="https://github.com/samcolson4/">Github profile</a></p>
-      <p><a href="https://www.linkedin.com/samcolson">Linkedin</a></p>
+      <a href="https://github.com/samcolson4/react-catan">GitHub repo</a> | <a href="http://www.colsondesign.com">Project Portfolio</a> | <a href="https://www.linkedin.com/samcolson">LinkedIn</a>
     </div>
   );
 }
