5bba2726527a4e6614f1799cd5c2b279558386ee
samcolson4 <samcolson4@gmail.com>
2021-07-02 15:42:37 +0100

    Remove unused vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
 import Header from './header/Header.js'
-import TextGenerator from './generator/textGenerator/textGenerator.js'
 import ImageGenerator from './generator/imageGenerator/imageGenerator.js'
 import About from './about/About.js'
 import './app.css'
 
-let text = <TextGenerator />
 let images = <ImageGenerator />
 
 function App() {
